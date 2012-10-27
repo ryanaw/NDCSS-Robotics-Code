369eaaad6b950752fc1a24fdafb69ff5ac3214b7
Ryan Williams <ryan@lifeisraw.me>
2012-10-27 17:23:57 -0400

    added MyRobotSimple.java

--- MyRobotSimple.java
+++ MyRobotSimple.java
@@ -1,18 +1,25 @@
  /*
- *        Name: Arshan Alam, Ryan Williams, Karim Guirguis, Ian Gregor, John Brunelle
+ *		  Program: Simplified Robot Controller
+ *   
+ *        Authors: Arshan Alam, Ryan Williams, Karim Guirguis, Ian Gregor, John Brunelle
  * 
  *        Date: Thursday, October 25, 2012
  * 
- * Description: This program Demonstrates the use of motors,
- * 				light sensors, and basic turning.
+ * 		  Description: This program Demonstrates the use of motors,
+ * 						light sensors, and basic turning.
  * 
  * */
-
 import lejos.nxt.*;	// Import of entire package
 
-public class MyRobot
+public class MyRobotSimple
 {
-
+	// Enumerate statues for robot
+	// ------------------------------------------------------------------------------------------------------------//
+	public enum NXJStatus {
+		NXJStatusOkay, NXJStatusOffTrack, NXJStatusShouldTurnLeft, NXJStatusShouldTurnRight
+	}
+	// ------------------------------------------------------------------------------------------------------------//
+	
 	// Method to check if the robot is completely off track
 	// ------------------------------------------------------------------------------------------------------------//
 	static boolean robotOffTrack(LightSensor leftSensor, LightSensor rightSensor, int leftInitialColor, int rightInitialColor, int range)
@@ -34,7 +41,16 @@ public class MyRobot
 				);	// end of return of boolean
 	}	// end of method isSensorOff();
 	// ------------------------------------------------------------------------------------------------------------//
-	
+	static NXJStatus updateStatus(LightSensor leftSensor, LightSensor rightSensor, int leftInitialColor, int rightInitialColor, int range){
+		NXJStatus returnStatus = NXJStatus.NXJStatusOkay; // Initialize the returned status as okay
+		if (robotOffTrack(leftSensor, rightSensor, leftInitialColor, rightInitialColor, range))
+			returnStatus = NXJStatus.NXJStatusOffTrack; // The robot is off track
+		else if (isSensorOff(rightSensor, rightInitialColor, range))
+			returnStatus = NXJStatus.NXJStatusShouldTurnLeft; // The right sensor is off
+		else if (isSensorOff(leftSensor, leftInitialColor, range))
+			returnStatus = NXJStatus.NXJStatusShouldTurnRight; // The left sensor is off
+		return returnStatus;
+	}
 	// Method to reset all Motor values to default
 	// ------------------------------------------------------------------------------------------------------------//
 	static void resetDefaultMotors(NXTMotor leftMotor, NXTMotor middleMotor, NXTMotor rightMotor, int defaultSpeed)
@@ -48,7 +64,7 @@ public class MyRobot
 		leftMotor.forward();
 		rightMotor.forward();
 		middleMotor.backward();	// Motor is attached Backwards
-		//-------------------------------------------------//	
+		//-------------------------------------------------//
 	}
 	// ------------------------------------------------------------------------------------------------------------//
 
@@ -69,76 +85,52 @@ public class MyRobot
 
 		Thread.sleep(5000);	// Program requirement of delay for 5 seconds
 
-
-		int rightColor = rightSensor.getLightValue();	// getting initial tracks light values
-		int leftColor = leftSensor.getLightValue();	// getting initial tracks light values
+		int rightColor = rightSensor.getLightValue(); // getting initial tracks light values
+		int leftColor = leftSensor.getLightValue(); // getting initial tracks light values
 		final int REVERSE_SPEED = 50; // Speed of robot when reversing
 		final int MAX_SPEED = 100;	// Maximum speed percentage Constant
 		final int SENSOR_RANGE = 4;	// Range the sensors will detect the tracks colors
 		final int TURNING_DIVISOR = 3;	// Divisor for the dividend MAX_SPEED, used when turning
 
-
 		// Infinite While loop for the program
 		while(true)
 		{
+			NXJStatus robotStatus = updateStatus(leftSensor, rightSensor, leftColor, rightColor, SENSOR_RANGE);
+			switch (robotStatus) {
+				case NXJStatusOffTrack:
+					while(updateStatus(leftSensor, rightSensor, leftColor, rightColor, SENSOR_RANGE) == NXJStatus.NXJStatusOffTrack)
+					{
+						leftMotor.setPower(REVERSE_SPEED);
+						rightMotor.setPower(REVERSE_SPEED);
+										
+						leftMotor.backward();
+						rightMotor.backward();
+					}
+					break;
+				case NXJStatusShouldTurnLeft:
+				case NXJStatusShouldTurnRight:
+					while (updateStatus(leftSensor, rightSensor, leftColor, rightColor, SENSOR_RANGE) == NXJStatus.NXJStatusShouldTurnLeft ||
+							updateStatus(leftSensor, rightSensor, leftColor, rightColor, SENSOR_RANGE) == NXJStatus.NXJStatusShouldTurnRight) 
+					{
+						// Ternary operator to choose the motor on the outside of the turn
+						NXTMotor primaryTurnMotor = (robotStatus == NXJStatus.NXJStatusShouldTurnLeft) ? rightMotor : leftMotor;
+						// Ternary operator to choose the motor on the inside of the turn
+						NXTMotor secondaryTurnMotor = (robotStatus == NXJStatus.NXJStatusShouldTurnLeft) ? leftMotor : rightMotor;
+						
+						secondaryTurnMotor.setPower(MAX_SPEED/TURNING_DIVISOR);
+						secondaryTurnMotor.backward();
 
-			// Method called to reset all motor values to default
-			//-----------------------------------------------------------//
-			resetDefaultMotors(leftMotor, middleMotor, rightMotor, MAX_SPEED);
-			//-----------------------------------------------------------//
-
-			// Selective structure if the right light sensor is off track
-			//-----------------------------------------------------------//
-			if (isSensorOff(rightSensor, rightColor, SENSOR_RANGE))
-			{
-				// Conditional while loop to turn robot back onto track
-				while(isSensorOff(rightSensor, rightColor, SENSOR_RANGE))
-				{
-					leftMotor.setPower(MAX_SPEED/TURNING_DIVISOR);	// Slow down the Left Motor
-					leftMotor.backward();	// Reverse the direction of rotation of the motor
-
-					rightMotor.setPower(MAX_SPEED);	// set Right Motor to Max Speed
-					rightMotor.forward();	// Set Right Motor Forward
-				}	// end of conditional while loop
-
-			}	// end of right sensor off selective structure
-			//-----------------------------------------------------------//
-
-			// Selective structure if the left light sensor is off track
-			//-----------------------------------------------------------//
-			if (isSensorOff(leftSensor, leftColor, SENSOR_RANGE))
-			{
-
-				// Conditional while loop to turn robot back onto track
-				while(isSensorOff(leftSensor, leftColor, SENSOR_RANGE))
-				{
-					rightMotor.setPower(MAX_SPEED/TURNING_DIVISOR);	// Slow down the Right Motor
-					rightMotor.backward();	// Reverse the direction of rotation of the motor
-
-					leftMotor.setPower(MAX_SPEED);	// set Left Motor to Max Speed
-					leftMotor.forward();	// set Left Motor Forward
-				} // end of conditional while loop
-
-			}	// end of left sensor off selective structure
-			//-----------------------------------------------------------//
-
-			
-			// Selective structure if the both light sensor are off track
-			//-----------------------------------------------------------//
-			if (robotOffTrack(leftSensor, rightSensor, leftColor, rightColor, SENSOR_RANGE))
-			{
-				// Conditional while loop for robot off track
-				while(robotOffTrack(leftSensor, rightSensor, leftColor, rightColor, SENSOR_RANGE))
-				{
-					leftMotor.setPower(REVERSE_SPEED);
-					rightMotor.setPower(REVERSE_SPEED);
-					
-					leftMotor.backward();
-					rightMotor.backward();
-				}	// end of conditional while loop for robot off track
-				
-			}	// End of robot off track selective structure
-			
+						primaryTurnMotor.setPower(MAX_SPEED);
+						primaryTurnMotor.forward();
+					}
+					break;
+				default:
+					// Method called to reset all motor values to default
+					//-----------------------------------------------------------//
+					resetDefaultMotors(leftMotor, middleMotor, rightMotor, MAX_SPEED);
+					//-----------------------------------------------------------//
+					break;
+			}
 		}	// end of Applications Infinite While Loop
 
 	}	// end of main method
